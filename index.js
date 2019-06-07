eac1f62669759673e4ae8104afe51d162c33906f
youssefhamdihh <youssef.h.h@hotmail.com>
2019-06-07 17:05:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,24 +24,34 @@ recognition.onspeechend = function()
 recognition.onresult = function(event)
 {
     const current = event.resultIndex;
-    const transcript = event.results[current][0].transcript;
+    var res = event.results[current][0].transcript;
+    const transcript = res.toLowerCase();
+    
     if(transcript.includes('apple')&&transcript.includes('select'))
+    {
     document.getElementById('apple').checked = true;
-    if(transcript.includes('apple')&&transcript.includes('deselect'))
+    }
+    else if(transcript.includes('apple')&&transcript.includes('deselect')){
     document.getElementById('apple').checked = false;
-    if(transcript.includes('orange')&&transcript.includes('select'))
+    }
+    else if(transcript.includes('orange')&&transcript.includes('select')){
     document.getElementById('orange').checked = true;
-    if(transcript.includes('orange')&&transcript.includes('deselect'))
+    }
+    else if(transcript.includes('orange')&&transcript.includes('deselect')){
     document.getElementById('orange').checked = false;
-    if(transcript.includes('grapes')&&transcript.includes('select'))
+    }
+    else if(transcript.includes('grapes')&&transcript.includes('select')){
     document.getElementById('grapes').checked = true;
-    if(transcript.includes('grapes')&&transcript.includes('deselect'))
+    }
+    else if(transcript.includes('grapes')&&transcript.includes('deselect')){
     document.getElementById('grapes').checked = false;
+    }
     if(transcript.includes('strawberries')&&transcript.includes('select'))
     document.getElementById('strawberries').checked = true;
     if(transcript.includes('strawberries')&&transcript.includes('deselect'))
     document.getElementById('strawberries').checked = false;
     respond();
+    console.log(transcript);
 };
 
 function respond()
